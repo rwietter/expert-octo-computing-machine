11675a7a0d03acd0ac8bd37c5f61fcfcd151ef08
rwietter <rwietter@zohomail.com>
2025-02-01 22:41:50 -0300

    Add metadata for bibliometric study application in page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,27 @@
 // app/page.js
-import React from 'react';
-import Search from './Search';
+import { Metadata } from 'next';
 import Header from './Header';
-import { useArticles } from './states/articles';
-import Results from './Results';
 import UniversityMap from './Map';
+import Results from './Results';
+import Search from './Search';
+
+export const Matadata: Metadata = {
+  title: 'Estudo Bibliométrico em IA',
+  description: 'Este App é um mecanismo de busca para estudos bibliométricos na área de Inteligência Artificial. Neste app, existem duas funcionalidades principais: um mecanismo de busca e um mapa com universidades, autores e artigos. O mecanismo de busca permite que os usuários pesquisem artigos por título, autor ou universidade. O mapa mostra a localização de universidades, autores e artigos.',
+  category: 'Bibliometria',
+  keywords: ['IA', 'Bibliometria', 'Inteligência Artificial'],
+  classification: 'Public',
+  applicationName: 'Estudo Bibliométrico em IA',
+  generator: 'Next.js',
+  robots: 'noindex, nofollow',
+  icons: [
+    {
+      url: '/favicon.ico',
+      sizes: '64x64',
+      type: 'image/png',
+    },
+  ],
+}
 
 // This App is a search engine for research bibliometric studies in the field of Artificial Intelligence. In this app, there are two main features: a search engine and a map with universities, authors, and articles. The search engine allows users to search for articles by title, author, or university. The map shows the location of universities, authors, and articles.
 
